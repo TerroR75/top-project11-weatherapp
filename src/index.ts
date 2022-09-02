005711c96bfd0bbd3a79b825fd0520086552e5f3
TerroR75 <kamilsulimierski@gmail.com>
2022-09-02 21:47:33 +0200

    dom rendering basic functionality

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ const API_KEY = process.env.WEATHER_APP_API_KEY;
 const inputCity = document.querySelector('.input-group .form-control') as HTMLInputElement;
 const searchCityBtn = document.querySelector('.input-group button')!;
 const weatherDisplay = document.querySelector('.weather-window') as HTMLElement;
+const errorMsg = document.querySelector('.app .input-error-msg') as HTMLElement;
 
 let searchedCity: string = '';
 let jsonData: string = '';
@@ -18,13 +19,38 @@ searchCityBtn.addEventListener('click', async () => {
 
         const fetchedData = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${inputCity.value}&APPID=${API_KEY}`, { mode: 'cors' });
         fetchedData.json().then(function (response) {
-            jsonData = response;
-            console.log(jsonData);
+            jsonData = JSON.stringify(response);
+            domFunc.renderWeatherInfo(weatherDisplay, jsonData);
         });
     };
 
     // Render weather info in DOM
-    domFunc.renderWeatherInfo(weatherDisplay, jsonData);
+
 });
+
+
+
+
+
+
+
+// ERROR message handling
+export function setErrorMessage(msg: string) {
+    errorMsg.innerText = msg;
+}
+
+export function toggleErrorMessage(visible: boolean = true) {
+    if (visible === true) {
+        errorMsg.classList.remove('error-hidden');
+    }
+    else {
+        if (errorMsg.classList.contains('error-hidden')) {
+            return;
+        }
+        else {
+            errorMsg.classList.add('error-hidden');
+        }
+    }
+}
 
 
--- src/scripts/renderDOM.ts
+++ src/scripts/renderDOM.ts
@@ -1,3 +1,39 @@
-export function renderWeatherInfo(DOMElement: HTMLElement, jsonData: string) {
+export function renderWeatherInfo(DOMElement: HTMLElement, jsonData: string, tempType: string = 'C') {
+    DOMElement.innerHTML = '';
 
+    // console.log(jsonData);
+    let cityData = JSON.parse(jsonData);
+
+    let degrees = calculateTemp(cityData.main.temp, tempType).toFixed(0);
+    const topPart = document.createElement('div');
+    topPart.classList.add('weather-top-part');
+    DOMElement.appendChild(topPart);
+
+    const cityName = document.createElement('h5');
+    cityName.innerText = cityData.name;
+    topPart.appendChild(cityName);
+
+    const degreeDisplay = document.createElement('div');
+    degreeDisplay.classList.add('degree-display');
+    topPart.appendChild(degreeDisplay);
+
+    const leftDegreeDisplay = document.createElement('div');
+    leftDegreeDisplay.classList.add('left-deg-dis');
+    const lddSpan = document.createElement('span');
+    lddSpan.innerHTML = `${degrees}&#176;${tempType.toUpperCase()}`;
+    degreeDisplay.appendChild(leftDegreeDisplay);
+    leftDegreeDisplay.appendChild(lddSpan);
+
+}
+
+function calculateTemp(degrees: number, tempType: string) {
+    if (tempType === 'C' || tempType === 'c') {
+        return degrees - 273.15;
+    }
+    else if (tempType === 'F' || tempType === 'f') {
+        return 1.8 * (degrees - 273.15) + 32;
+    }
+    else {
+        return degrees;
+    }
 }
